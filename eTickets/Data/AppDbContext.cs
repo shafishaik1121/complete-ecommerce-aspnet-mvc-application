1e3e3fc7a7b6926cf6a918c67bb69b29288ca0ec
DESKTOP-DAQQ80D\shafi shaik <shafi.shaik@quantifeed.com>
2023-03-11 18:07:36 +0530

    PaypalCommit

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -29,6 +29,12 @@ namespace eTickets.Data
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
+
+        //Order related tables
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
         
     }
 }
---
+++ eTickets/Data/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> item, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrderByUserIdAsync(string userId);
+    }
+}
