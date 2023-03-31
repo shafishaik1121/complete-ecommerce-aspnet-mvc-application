cc8dc8db85dbcbbf2a239f8693cff6d87972e44b
ShafiShaik <shaikshafi1121@gmail.com>
2023-03-31 20:36:29 +0530

    changes1commits

--- eTickets/wwwroot/js/site.js
+++ eTickets/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    var PlaceHolderElement = $('#PlaceHolderHere');
+    $('button[data-toggle="ajax-model"]').click(function (event) {
+        var url = $(this).data('url');
+        var decodedUrl = decodeURIComponent(url);
+        $.get(decodedUrl).done(function (data) {
+            PlaceHolderElement.html(data);
+            PlaceHolderElement.find('.model').model('show');
+        })
+    })
+
+    PlaceHolderElement.on('click', '[data-save="model"]', function (event) {
+
+        var form = $(this).parents('.model').find('form');
+        var actionUrl = form.attr('action');
+        var url = "/Movies/" + actionUrl;
+        var sendData = form.serialize();
+        $.post(url, sendData).done(function (data) {
+            PlaceHolderElement.find('.model').model('hide');
+        })
+    })
+})
